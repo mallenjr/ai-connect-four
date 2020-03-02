b97ae6c7b0d6b1dd931c463015ef62b74ef27f10
derekwilson99 <61640678+derekwilson99@users.noreply.github.com>
2020-03-01 18:24:47 -0600

    Consolidated most of gameBoard.js and NodeClass.js

--- NodeClass.js
+++ NodeClass.js
@@ -13,4 +13,57 @@ export class NodeClass {
   set ownership(player) {
     this.player = player;
   }
+
+  placeChecker(player, gameBoard) {
+    // eslint-disable-next-line no-param-reassign
+    gameBoard[this.column - 1][this.row - 1].player = player;
+  }
+
+  isRight(gameBoard) {
+    let flag = false;
+    flag = gameBoard[this.column - 1][this.row].player === 1;
+    return flag;
+  }
+
+  isLeft(gameBoard) {
+    let flag = false;
+    flag = gameBoard[this.column - 1][this.row - 2].player === 1;
+    return flag;
+  }
+
+  isBelow(gameBoard) {
+    let flag = false;
+    flag = gameBoard[this.column][this.row - 1].player === 1;
+    return flag;
+  }
+
+  isAbove(gameBoard) {
+    let flag = false;
+    flag = gameBoard[this.column - 2][this.row - 1].player === 1;
+    return flag;
+  }
+
+  isBelowAndRight(gameBoard) {
+    let flag = false;
+    flag = gameBoard[this.column][this.row].player === 1;
+    return flag;
+  }
+
+  isBelowAndLeft(gameBoard) {
+    let flag = false;
+    flag = gameBoard[this.column][this.row - 2].player === 1;
+    return flag;
+  }
+
+  isAboveAndRight(gameBoard) {
+    let flag = false;
+    flag = gameBoard[this.column - 2][this.row].player === 1;
+    return flag;
+  }
+
+  isAboveAndLeft(gameBoard) {
+    let flag = false;
+    flag = gameBoard[this.column - 2][this.row - 2].player === 1;
+    return flag;
+  }
 }
